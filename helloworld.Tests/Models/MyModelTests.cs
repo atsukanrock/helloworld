c812a794d07d6f4d229c593700b164435d083432
atsukanrock <atsukanrock@gmail.com>
2015-06-21 22:32:11 +0900

    Add test project to helloworld project.

---
+++ helloworld.Tests/Models/MyModelTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace helloworld.Models
+{
+	[TestFixture]
+	public class MyModelTests
+	{
+		[TestCase("atsukanrock", "Hello, atsukanrock!!")]
+		[TestCase("Sansan", "Hello, Sansan!!")]
+		public void GreetShouldWorkAsExpected(string name, string expected)
+		{
+			var that = new MyModel();
+			var actual = that.Greet(name);
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void GreetShouldThrowArgumentNullExceptionIfNameIsNull()
+		{
+			var that = new MyModel();
+			Assert.Throws<ArgumentNullException>(() => that.Greet(null));
+		}
+	}
+}
---
+++ helloworld/Models/MyModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace helloworld.Models
+{
+	public class MyModel
+	{
+		public string Greet(string name)
+		{
+			if(name == null) throw new ArgumentNullException("name");
+
+			return "Hello, " + name + "!!";
+		}
+	}
+}
